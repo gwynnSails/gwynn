54cd5991d2a6f84e7f7f24c73172ad6ae8f3fc4f
lucas <fake@psuedo.net>
2015-08-03 14:28:14 -0400

    new commit

---
+++ api/controllers/queryController.js
@@ -0,0 +1,20 @@
+module.exports = {
+ attributes: {
+    Question: {
+      type: 'string',
+      required: true
+    },
+
+    Value: {
+      type: 'string'
+    },
+
+    count: {
+      type: 'string'
+    },
+
+    Date: {
+      type: 'string'
+    }
+  }
+};
--- config/models.js
+++ config/models.js
@@ -17,7 +17,8 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  //connection: 'jemaMongodbServer',
+  connection: 'local_metricsMongoDB' //,
+  // connection: 'remote_jemaMongoDB' //,
 
   /***************************************************************************
   *                                                                          *
