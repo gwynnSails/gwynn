550f6f67939556a00fe67143978ca544f2e011f6
lucas <fake@psuedo.net>
2015-08-05 12:56:27 -0400

    some updates

--- assets/js/qMetrics.js
+++ assets/js/qMetrics.js
@@ -1,2 +1,35 @@
 
-//var cursor = db.drives.find({},{"_id":1}).sort({"driveDate":1}).limit(1);
+function subtractDays(date, days) {
+  var result = new Date(date);
+  result.setDate(date.getDate() - days);
+  return result;
+}
+
+$.getJSON("http://localhost:1337/Query/find?question=total&value=users", callbackGetUsers);
+$.getJSON("http://localhost:1337/Query/find?question=total&value=models", callbackGetModels);
+$.getJSON("http://localhost:1337/Query/find?question=total&value=modelRuns", callbackGetRuns);
+
+function callbackGetUsers(data) {
+  var milkUsers = JSON.stringify(data)
+  document.querySelector('.monthlyUniqueUsers').innerHTML = milkUsers;
+}
+
+function callbackGetModels(data) {
+  var milkModels = JSON.stringify(data);
+  document.querySelector('.monthlyUniqueModels').innerHTML = milkModels;
+}
+
+function callbackGetModels(data) {
+  var milkRuns = JSON.stringify(data);
+  document.querySelector('.monthlyModelRuns').innerHTML = milkRuns;
+}
+
+var jqxhr = $.getJSON("http://localhost:1337/Query/find?question=total",
+  function(count) {
+    $.each(count, function(i, item) {
+      alert(item.count)
+    });
+  });
+  .success(function() {alert("sucess"); })
+  .error(function() {alert("error"); })
+  .complete(function() {alert("complete"); });
